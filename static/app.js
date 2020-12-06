7477b6a25a777e7bbbeb2bbc3336632804300f3f
Stephen <stephen.lyssy@gmail.com>
2020-12-06 14:09:11 -0600

    Updated index.html and app.js in an effor to build the table for the dashboard.

--- static/app.js
+++ static/app.js
@@ -1,6 +1,21 @@
 console.log("Hello")
-d3.json("/").then(
-    data=> {
-        console.log(data)
+
+buildTable(project_list)
+
+function buildTable(data){
+    let table = document.getElementById('#dashboardTable')
+
+    for (var i = 0; i < data.length; i++){
+        var row = `<tr>
+                        <td>${data[i].name}</td>
+                        <td>${data[i].revenue}</td>
+                        <td>${data[i].est_labor_hours}</td>
+                        <td>${data[i].est_labor_hours}</td>
+                        <td>${data[i].est_labor_expense}</td>
+                        <td>${data[i].est_labor_expense}</td>
+                        <td>${data[i].act_start_date}</td>
+                   </tr>`
+        
+        table.innerHTML += row
     }
-)
+}
