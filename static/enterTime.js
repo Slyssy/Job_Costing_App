eadde64ab6e041c2fa14afbee746210b1c63919e
jgandre <67715755+jgandre@users.noreply.github.com>
2020-12-09 23:41:43 -0600

    select proj dropdown menu working on enterTime page.

--- static/enterTime.js
+++ static/enterTime.js
@@ -12,3 +12,13 @@ for(var i = 0; i < employees.length; i++) {
 };
 
 
+// Select Project Name
+var projects = dropdown_dict.project_list.map(d =>d.name);
+var ele = document.getElementById("proName");
+for(var i = 0; i < projects.length; i++) {
+  var opt = projects[i];
+  var el = document.createElement("option");
+  el.text = opt;
+  el.value = opt;
+  ele.add(el);
+};
