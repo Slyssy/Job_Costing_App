d55990faa615519f2fbf144a2e82527c44ac0799
Stephen <stephen.lyssy@gmail.com>
2020-12-10 21:08:30 -0600

    Fixed the frontend piece of loading the project details to the search.html page for each project.

--- static/app.js
+++ static/app.js
@@ -55,7 +55,7 @@ function buildTable(data) {
 
   for (const item in data) {
     var row = `<tr>
-                        <td><a href="/search?name=${data[item].id}">${data[item].project_name}</a></td>
+                        <td><a href="/search?project_id=${data[item].id}">${data[item].project_name}</a></td>
                         <td>${data[item].fin_act_revenue}</td>
                         <td>${data[item].fin_est_labor_hours}</td>
                         <td>${data[item].fin_act_labor_hours}</td>
