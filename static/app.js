f4738c69911b9ea361f82b28cd2508474c73a370
Stephen <stephen.lyssy@gmail.com>
2020-12-30 14:53:30 -0600

    Updated table script so that table could be sorted when loaded.

--- static/app.js
+++ static/app.js
@@ -22,30 +22,60 @@ function searchTable(value, data) {
 }
 
 // Begin Build Table Javascript
-buildTable(project_list);
+// buildTable(project_list);
 
-function buildTable(data) {
-  let table = document.getElementById("dashboardTable");
+// function buildTable(data) {
+//   let table = document.getElementById("dashboardTable");
 
-  table.innerHTML = "";
+//   table.innerHTML = "";
 
-  for (const item in data) {
-    var row = `<tr>
+//   for (const item in data) {
+//     var row = `<tr>
 
-                        <td><a href="/search?project_id=${data[item].id}">${data[item].project_name}</a></td>
+//                         <td><a href="/search?project_id=${data[item].id}">${data[item].project_name}</a></td>
 
-                        <td>$ ${data[item].fin_act_revenue}</td>
-                        <td>${data[item].fin_est_labor_hours}</td>
-                        <td>${data[item].fin_act_labor_hours}</td>
-                        <td>$ ${data[item].fin_est_labor_expense}</td>
-                        <td>$ ${data[item].fin_act_labor_expense}</td>
-                        <td>${data[item].act_start_date}</td>
-                   </tr>`;
+//                         <td>$ ${data[item].fin_act_revenue}</td>
+//                         <td>${data[item].fin_est_labor_hours}</td>
+//                         <td>${data[item].fin_act_labor_hours}</td>
+//                         <td>$ ${data[item].fin_est_labor_expense}</td>
+//                         <td>$ ${data[item].fin_act_labor_expense}</td>
+//                         <td>${data[item].act_start_date}</td>
+//                    </tr>`;
 
-    table.innerHTML += row;
+//     table.innerHTML += row;
+//   }
+// }
+// for (const item in project_list) {
+//   // console.log(item)
+//   // console.log(project_list[item])
+
+//   // const project_item = project_list[item]
+//   // console.log(project_item)
+
+//   // const projects = [project_item]
+//   // console.log(projects)
+// }
+
+let projectArray = Object.keys(project_list).map((i) => project_list[i]);
+console.log(projectArray);
+
+projectArray.sort((a, b) => a.project_name !== b.project_name ? a.project_name < b.project_name ? -1 : 1 : 0);
+
+buildTable(projectArray)
+
+function buildTable(data){
+  let table = document.getElementById('dashboardTable')
+
+  for (let i = 0; i < data.length; i++){
+    let  row = `<tr>
+                             <td><a href="/search?project_id=${data[i].id}">${data[i].project_name}</a></td>
+                              <td>${data[i].fin_act_revenue}</td>
+                              <td>${data[i].fin_est_labor_hours}</td>
+                              <td>${data[i].fin_act_labor_hours}</td>
+                              <td>$ ${data[i].fin_est_labor_expense}</td>
+                              <td>$ ${data[i].fin_act_labor_expense}</td>
+                              <td>${data[i].act_start_date}</td>
+                          </tr>`
+       table.innerHTML += row   
   }
-}
-for (const item in project_list) {
-  // console.log(item)
-  // console.log(project_list[item])
 }
